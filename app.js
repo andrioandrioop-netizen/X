2ba99e84be7ab53ff8cfcff3b098073429f26f66
andrioandrioop-netizen <andrioandrioop@gmail.com>
2026-04-16 17:23:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-storage.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCs8xpXbpp2CkCp9y_wjRlo1or0VcNcZv8",
-  authDomain: "experimental-29ccd.firebaseapp.com",
-  projectId: "experimental-29ccd",
-  storageBucket:"experimental-29ccd.firebasestorage.app",
-};
-
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-
-window.uploadFile = async function () {
-  const fileInput = document.getElementById("fileInput");
-  const file = fileInput.files[0];
+async function uploadFile() {
+  const file = document.getElementById("fileInput").files[0];
+  const status = document.getElementById("status");
 
   if (!file) {
-    alert("Select a file first!");
+    alert("Select file");
     return;
   }
 
-  const status = document.getElementById("status");
   status.innerText = "Uploading...";
 
-  try {
-    const storageRef = ref(storage, "uploads/" + Date.now() + "_" + file.name);
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("upload_preset", "YOUR_UPLOAD_PRESET");
 
-    await uploadBytes(storageRef, file);
+  const res = await fetch(
+    "https://api.cloudinary.com/v1_1/YOUR_CLOUD_NAME/auto/upload",
+    {
+      method: "POST",
+      body: formData
+    }
+  );
 
-    const url = await getDownloadURL(storageRef);
+  const data = await res.json();
 
-    status.innerText = "Upload successful!";
+  status.innerText = "Done";
 
-    const link = document.getElementById("downloadLink");
-    link.href = url;
-    link.innerText = "Download File";
-
-  } catch (err) {
-    console.error(err);
-    status.innerText = "Upload failed";
-  }
-};
+  const link = document.getElementById("link");
+  link.href = data.secure_url;
+  link.innerText = "Open File";
+    }
